af6c9cbeeb937d634a8d3c62f082781de2959816
Hong Zhang <phossa@126.com>
2015-10-30 13:05:02 +0800

    added files
    added files

---
+++ src/Phossa/Query/Message/Message.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Phossa Project
+ *
+ * @see         http://www.phossa.com/
+ * @copyright   Copyright (c) 2015 phossa.com
+ * @license     http://mit-license.org/ MIT License
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa\Query\Message;
+
+use Phossa\Shared\Message\MessageAbstract;
+
+/**
+ * Message class for Phossa\Query
+ *
+ * @package \Phossa\Query
+ * @author  Hong Zhang <phossa@126.com>
+ * @version 1.0.0
+ * @since   1.0.0 added
+ */
+class Message extends MessageAbstract
+{
+    /**#@+
+     * @type  int
+     */
+
+    /**
+     * Wrong log level name
+     */
+    const WRONG_LOG_LEVEL       = 1510301240;
+
+    /**#@-*/
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $messages = [
+    ];
+}
